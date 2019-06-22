3d4c8fb0e3dd4c90c7494faa933175b9e4333370
rakeshnambiar <rakeshnambiar.c@gmail.com>
2019-06-22 20:37:53 +0100

    script file added

---
+++ run-regression-tests.sh
@@ -0,0 +1,14 @@
+docker-compose down
+docker-compose up -d selenium zalenium 
+docker-compose up --exit-code-from nodeSetup
+
+
+if [ $? = 0 ]; then
+  docker-compose down
+  printf '%s\n' "Test Pass"
+  exit 0
+else
+   docker-compose down
+   printf '%s\n' "Test Failed"
+   exit 1
+fi
